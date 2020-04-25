2edbd9e396abd5ba6f716d01228458e08e97dfb8
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-25 10:00:58 -0400

    added mongodb uri

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
